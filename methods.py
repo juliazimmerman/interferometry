5e58c403c16de6598691a157f48599f3c30e071b
Julia <julia.s.zimmerman@gmail.com>
2025-07-14 15:30:41 -0700

    s unit vector calculation

--- methods.py
+++ methods.py
@@ -1,8 +1,7 @@
 # All packages used
-from astropy.coordinates import EarthLocation
+from astropy.coordinates import EarthLocation, AltAz, SkyCoord
 from astropy.time import Time
 from astropy import units as u
-from astropy.coordinates import SkyCoord
 import numpy as np
 
 # EarthLocation Method
@@ -10,26 +9,34 @@ def create_earth_location():
     lon = 121
     lat = 33
     location = EarthLocation.from_geodetic(lon, lat)
-    print(location)
+    return location
 
 # Time Method
 def time_of_observation():
     times = ['2025-07-11T14:33:00']
     observation_time = Time(times)
-    print(observation_time)
+    return observation_time
 
-# SkyCoord Method
+# SkyCoord Method - adjust ra and dec parameters for each csv file
 def creating_sky_coordinate():
     data = np.loadtxt('stars.csv', delimiter=',', usecols=(1,2), skiprows=1)
-    ra = data[0, 0]
+    ra = data[0, 0] 
     dec = data[0, 1]
     coordinate = SkyCoord(ra*u.deg, dec*u.deg, frame='icrs', obstime=Time([2000, 2010], format ='jyear'))
-    print(coordinate)
+    return coordinate
 
-def main():
-    create_earth_location()
-    time_of_observation()
-    creating_sky_coordinate()
+# ŝ Unit Vector Calculation
+def unit_vector_calculation():
+    # Take variables from previous functions
+    icrs_coordinate = creating_sky_coordinate()
+    observation_time = time_of_observation()
+    obslocation = create_earth_location()
+    # Transform our SkyCoord to AltAz coordinates
+    altaz_coordinate = icrs_coordinate.transform_to(AltAz(obstime=observation_time, location=obslocation))
+    enu_coordinates = altaz_coordinate.cartesian
+    
 
-if __name__ == "__main__":
-    main()
+create_earth_location()
+time_of_observation()
+creating_sky_coordinate()
+unit_vector_calculation()
