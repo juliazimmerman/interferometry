fa7ef24556df56475ebd6958fb6677d9b4d05b18
Julia <julia.s.zimmerman@gmail.com>
2025-07-15 16:24:10 -0700

    added baseline vector calculation

--- methods.py
+++ methods.py
@@ -24,14 +24,23 @@ def creating_sky_coordinate():
     data = np.loadtxt('stars.csv', delimiter=',', usecols=(1,2), skiprows=1)
     ra = data[:, 0]
     dec = data[:, 1]
-    coordinate = SkyCoord(ra*u.deg, dec*u.deg, frame='icrs')
+    coordinate = SkyCoord(ra[0]*u.deg, dec[0]*u.deg, frame='icrs')
     return coordinate
 
+# An array to store positions of antennas
 def antenna_positions_array():
     a = np.zeros(shape=(3,2)) # Change second parameter to number of antennas
     # Syntax: a[row, column] = value. Defaults to 0 without specified value.
     a[0, 1] = 10
     return a
+
+# Calculate the baseline vector
+def base_line_vector():
+    antenna_array = antenna_positions_array()
+    antenna_1 = antenna_array[:, 0]
+    antenna_2 = antenna_array[:, 1]
+    b = antenna_2 - antenna_1
+    return b
 
 # ŝ Unit Vector Calculation
 def unit_vector_calculation():
@@ -42,10 +51,12 @@ def unit_vector_calculation():
     # Transform our SkyCoord to AltAz then ENU coordinates
     altaz_coordinate = icrs_coordinate.transform_to(AltAz(obstime=observation_time, location=obslocation))
     enu_coordinates = altaz_coordinate.cartesian
-    return enu_coordinates
+    return enu_coordinates    
 
 def main():
     antenna_positions_array()
     unit_vector_calculation()
+    base_line_vector()
+    
 if __name__ == "__main__":
     main()
