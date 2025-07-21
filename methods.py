edd3c3aaf3ae023d55ffac09851129d7b892d25b
Julia <julia.s.zimmerman@gmail.com>
2025-07-21 14:46:42 -0700

    renamed y,z variables to duration, num_of_times

--- methods.py
+++ methods.py
@@ -15,8 +15,8 @@ def set_start_time(time):
 
 # Method for creating multiple times - use to create multipe time objects for a time array
 # base_time = starting time, x = starting value(string), y = ending value(integer), z = number of points to generate (integer)
-def multiple_times(base_time, y, z):
-    time_array = Time(base_time) + np.linspace(0, y, z) * u.hour
+def multiple_times(base_time, duration, num_of_times):
+    time_array = Time(base_time) + np.linspace(0, duration, num_of_times) * u.hour
     for i in range(len(time_array)):
         time_array[i] = Time(time_array[i])
     return time_array
@@ -39,10 +39,10 @@ def base_line_vector(antenna1, antenna2):
     return b
 
 # ŝ Unit Vector Calculation
-def unit_vector_calculation(ra, dec, base_time, y, z, lon, lat):
+def unit_vector_calculation(ra, dec, base_time, duration, num_of_times, lon, lat):
     # Take variables from previous functions
     icrs_coordinate = creating_sky_coordinate(ra, dec)
-    observation_time = multiple_times(base_time, y, z)
+    observation_time = multiple_times(base_time, duration, num_of_times)
     obslocation = create_earth_location(lon, lat)
     # Transform our SkyCoord to AltAz then ENU coordinates
     altaz_coordinate = icrs_coordinate.transform_to(AltAz(obstime=observation_time, location=obslocation))
