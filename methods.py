e53cc2501978ea56626e469b7d16d729a31912a4
Julia <julia.s.zimmerman@gmail.com>
2025-07-21 11:42:39 -0700

    function paramter edits

--- methods.py
+++ methods.py
@@ -9,6 +9,9 @@ def create_earth_location(lon, lat):
     location = EarthLocation.from_geodetic(lon, lat)
     return location
 
+a = create_earth_location(133, 29)
+print(a)
+
 # Method for creating a single time - use to create only one time object
 def set_start_time(time):
     return Time(time)
@@ -34,24 +37,19 @@ def antenna_positions_array(num_of_antennas, positions_list):
     return positions_array
 
 # Calculate the baseline vector
-def base_line_vector():
-    antenna_array = antenna_positions_array(2, [(50, 0, 0), (0, 100, 0)])
-    antenna_1 = antenna_array[0]
-    antenna_2 = antenna_array[1]
-    b = antenna_2 - antenna_1
+def base_line_vector(antenna1, antenna2):
+    b = antenna2 - antenna1
     return b
 
 # ŝ Unit Vector Calculation
-def unit_vector_calculation():
+def unit_vector_calculation(ra, dec, base_time, y, z, lon, lat):
     # Take variables from previous functions
-    icrs_coordinate = creating_sky_coordinate(128, -32.1) # Point on sky
-    observation_time = multiple_times('2025-07-18 13:00:00', 6, 10) # Observation Time
-    obslocation = create_earth_location(121, 33) # Observation Location
+    icrs_coordinate = creating_sky_coordinate(ra, dec)
+    observation_time = multiple_times(base_time, y, z)
+    obslocation = create_earth_location(lon, lat)
     # Transform our SkyCoord to AltAz then ENU coordinates
     altaz_coordinate = icrs_coordinate.transform_to(AltAz(obstime=observation_time, location=obslocation))
     enu_coordinates = altaz_coordinate.cartesian.xyz.value.T
-    return enu_coordinates
-
-
+    return enu_coordinates[0]
 
 
